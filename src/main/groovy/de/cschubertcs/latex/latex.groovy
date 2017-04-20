22bf5c933b88096fbe5e37d7dde56e3bf3362f77
Christoph <c.schubert.cs@gmail.com>
2017-04-20 15:16:27 +0200

    Fixed #2. All path references are now loaded lazy.

--- src/main/groovy/de/cschubertcs/latex/latex.groovy
+++ src/main/groovy/de/cschubertcs/latex/latex.groovy
@@ -21,7 +21,7 @@ class LatexPlugin implements Plugin<Project> {
   @Override
   void apply(Project project) {
     project.extensions.create("latex", LatexPluginExtension)
-    
+
     project.defaultTasks 'cook'
 
     project.task('check') {
@@ -30,15 +30,15 @@ class LatexPlugin implements Plugin<Project> {
         if (!isExecutableExisting("${project.latex.latexBinary}")) {
           throw new InvalidUserDataException("The binary '${project.latex.latexBinary}' is not present in the path.")
         }
-        
+
         println("Checking that '${project.latex.bibtexBinary}' is in the path system variable...")
         if (!isExecutableExisting("${project.latex.bibtexBinary}")) {
           throw new InvalidUserDataException("The binary '${project.latex.bibtexBinary}' is not present in the path.")
         }
-        
+
         println("Checking that the primary source file exists...")
-        if (!project.file("${project.latex.rawDirectory}/${project.latex.latexFile}").exists()) {
-          throw new InvalidUserDataException("The file '${project.latex.rawDirectory}/${project.latex.latexFile}' does not exist.")
+        if (!project.file("${project.latex.latexFilePath}").exists()) {
+          throw new InvalidUserDataException("The file '${project.latex.latexFilePath}' does not exist.")
         }
 
         println("Checking that the target directory exists (or creating it if necessary)...")
@@ -52,34 +52,43 @@ class LatexPlugin implements Plugin<Project> {
     (1..3).each { counter ->
       project.task("pdfLatexRun$counter", type: Exec, dependsOn: project.check) {
         doFirst {
-          println "Building ${project.latex.rawDirectory}/${project.latex.latexFile} into ${project.latex.tmpDirectory} via pdflatex."
+          println "Building ${project.latex.latexFilePath} into ${project.latex.tmpDirectory} via pdflatex."
         }
-        
+
         dependsOn project.check
-        
+
         if( counter > 2 ) {
           // the third run is only needed in case that there is a biber file
-          project."pdfLatexRun$counter".onlyIf{ project.file("${project.latex.tmpDirectory}/${project.latex.biberFile}").exists() }
+          project."pdfLatexRun$counter".onlyIf {
+            project.file("${->project.latex.biberConfigFilePath}").exists()
+          }
         }
-        
+
         standardOutput = new ByteArrayOutputStream() // stops output to STDOUT
 
         // TODO: check what this does
-        //outputs.upToDateWhen{false}
-    
-        workingDir '.' 
-        commandLine("pdflatex", "-output-directory", "${project.latex.tmpDirectory}", "${project.latex.rawDirectory}/${project.latex.latexFile}")
+//        inputs.files
+        outputs.upToDateWhen{false}
+
+        workingDir '.'
+        commandLine(
+          "pdflatex",
+          "-output-directory", "${->project.latex.tmpDirectory}",
+          "${->project.latex.latexFilePath}"
+        )
+
         ignoreExitValue true
 
         // TODO: check what this does
-        outputs.file project.file("${project.latex.tmpDirectory}/${project.latex.pdfFile}")
+        outputs.file project.file("${->project.latex.pdfFilePath}")
 
         doLast {
           if (execResult.exitValue != 0) {
             println(standardOutput.toString())
-            throw new GradleException("There was an error while executing the command '" + commandLine.join(" ") + "'. See the output above for more information.")
+            throw new GradleException("There was an error while executing the command '" +
+              commandLine.join(" ") + "'. See the output above for more information.")
           } else {
-            println "Built ${project.latex.rawDirectory}/${project.latex.latexFile} into ${project.latex.rawDirectory} via pdflatex."
+            println "Built ${project.latex.latexFilePath} into ${project.latex.rawDirectory} via pdflatex."
           }
         }
       }
@@ -87,23 +96,30 @@ class LatexPlugin implements Plugin<Project> {
 
     project.task('biber', type: Exec, dependsOn: project.check) {
       doFirst {
-        println "Building the bibliography of ${project.latex.rawDirectory}/${project.latex.latexFile} into ${project.latex.tmpDirectory} via biber."
+        println "Building the bibliography of ${project.latex.latexFilePath} into ${project.latex.tmpDirectory} via biber."
       }
-      
-      project.biber.onlyIf{ project.file("${project.latex.tmpDirectory}/${project.latex.biberFile}").exists() }
-      
+
+      project.biber.onlyIf{
+        project.file("${->project.latex.biberConfigFilePath}").exists()
+      }
+
       standardOutput = new ByteArrayOutputStream() // stops output to STDOUT
-      
-      workingDir '.' 
-      commandLine("biber", "-D", "--input-directory", "${project.latex.rawDirectory}", "${project.latex.tmpDirectory}/${project.latex.biberFile}")
+
+      workingDir '.'
+      commandLine(
+        "biber",
+        "-D",
+        "--input-directory", "${->project.latex.rawDirectory}",
+        "${->project.latex.biberConfigFilePath}"
+      )
       ignoreExitValue true
-      
+
       doLast {
         if (execResult.exitValue != 0) {
           println(standardOutput.toString())
           throw new GradleException("There was an error while executing the command '" + commandLine.join(" ") + "'. See the output above for more information.")
         } else {
-          println "Built the bibliography of ${project.latex.rawDirectory}/${project.latex.latexFile} into ${project.latex.tmpDirectory} via biber."
+          println "Built the bibliography of ${project.latex.latexFilePath} into ${project.latex.tmpDirectory} via biber."
         }
       }
     }
@@ -114,13 +130,13 @@ class LatexPlugin implements Plugin<Project> {
       dependsOn project.pdfLatexRun2
       dependsOn project.biber
       dependsOn project.pdfLatexRun3
-      
+
       project.pdfLatexRun2.mustRunAfter project.pdfLatexRun1
       project.biber.mustRunAfter project.pdfLatexRun2
       project.pdfLatexRun3.mustRunAfter project.biber
-      
+
       doLast {
-        println "Cooked ${project.latex.rawDirectory}/${project.latex.latexFile} to ${project.latex.tmpDirectory}/${project.latex.pdfFile}."
+        println "Cooked ${project.latex.latexFilePath} to ${project.latex.pdfFilePath}."
       }
     }
   }
@@ -129,10 +145,15 @@ class LatexPlugin implements Plugin<Project> {
 class LatexPluginExtension {
     def latexBinary = 'pdflatex'
     def bibtexBinary = 'biber'
+
     def documentBase = 'Test'
     def rawDirectory = 'src'
     def tmpDirectory = 'build'
+
     def latexFile = "${-> documentBase}.tex"
     def pdfFile = "${-> documentBase}.pdf"
-    def biberFile = "${-> documentBase}.bcf"
+    def biberConfigFile = "${-> documentBase}.bcf"
+    def latexFilePath = "${-> rawDirectory}/${-> latexFile}"
+    def pdfFilePath = "${-> tmpDirectory}/${-> pdfFile}"
+    def biberConfigFilePath = "${-> tmpDirectory}/${-> biberConfigFile}"
 }
